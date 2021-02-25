e7d6aaa360deb010e4966a996db2acab8cf8cb4b
Brian Clozel <bclozel@vmware.com>
2021-02-25 15:24:05 +0100

    Create sample for testing Gradle plugin
    Fixes gh-

---
+++ src/main/java/com/example/nativerunner/NativerunnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.nativerunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativerunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativerunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nativerunner/NativerunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nativerunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativerunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
